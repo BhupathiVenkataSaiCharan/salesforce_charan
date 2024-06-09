d7381a476c74876c9b96b724097a2082e34ff43c
BhupathiVenkataSaiCharan <venkatasaicharanbhupathi@gmail.com>
2024-06-09 21:32:46 +0530

    component event scenario

--- force-app/main/default/aura/compEventChild/compEventChildController.js
+++ force-app/main/default/aura/compEventChild/compEventChildController.js
@@ -1,11 +1,68 @@
 ({
-    callme : function(component, event, helper) {
-
+    callme: function(component, event, helper) {
+        // Get the account data from the component
         var a = component.get("v.cacc");
 
-        var evt = component.getEvent("sona");
+        // Create a server-side action to insert the account
+        var action = component.get("c.createAcc");
+        action.setParams({ "A": a });
 
-        evt.setParams({"eacc":a});
-        evt.fire(); 
+        // Set the callback for the action
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var result = response.getReturnValue();
+                if (result !== 'Error') {
+                    // Fire the custom event to refresh the account list in the parent component
+                    var evt = component.getEvent("sona");
+                    evt.fire();
+                }
+            } else {
+                console.log('Failed to create account: ' + response.getError());
+            }
+        });
+
+        // Enqueue the action
+        $A.enqueueAction(action);
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+/*({
+    callme : function(component, event, helper) {
+
+        // var a = component.get("v.cacc");
+
+        // var evt = component.getEvent("sona");
+
+        // evt.setParams({"eacc":a});
+        // evt.fire(); 
+
+        var a = component.get("v.cacc");
+        var evt = component.getEvent("sona");
+        var action = component.get("c.createAcc");
+        action.setParams({"A":a});
+
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state == 'SUCCESS'){
+                var result=response.getReturnValue();
+                if(result!= 'Error'){
+                    evt.fire();
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
+    }
+})
+*/
--- force-app/main/default/aura/compEventParent/compEventParentController.js
+++ force-app/main/default/aura/compEventParent/compEventParentController.js
@@ -1,4 +1,49 @@
 ({
+    doInit: function(component, event, helper) {
+        // Initialize the columns for the datatable
+        var columns = [
+            {label: 'Account Name', fieldName: 'Name'},
+            {label: 'Account Phone', fieldName: 'Phone', type: 'phone'},
+            {label: 'Account Industry', fieldName: 'Industry'}
+        ];
+        component.set("v.col", columns);
+
+        // Load existing accounts on initialization
+        var action = component.get("c.displayAcc");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                component.set("v.accounts", response.getReturnValue());
+            } else {
+                console.log('Failed to retrieve accounts: ' + response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showme: function(component, event, helper) {
+        // Retrieve and display the updated list of accounts
+        var action = component.get("c.displayAcc");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var accounts = response.getReturnValue();
+                component.set("v.accounts", accounts);
+            } else {
+                console.log('Failed to retrieve accounts: ' + response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
+
+
+
+
+
+
+/*
+({
     doInit : function(component, event, helper) {
 
         var c = [
@@ -10,9 +55,21 @@
     },
     showme: function (component,event,helper) {
         
+        /*
         var ea = event.getParam("eacc"); // Get data from event
-        var accounts = component.get("v.accounts"); // Get existing accounts
+        var accounts = component.get("v.accounts"); // Get existing accounts from component(first time null)
         accounts.push({ ...ea }); // Create a new object using the spread operator and push to list
         component.set("v.accounts", accounts); // Update component's account list
-    }
-})
+        */
+
+        // var action = component.get("c.displayAcc");
+        // action.setCallback(this,function(response){
+        //     var state = response.getState();
+        //     if(state == 'SUCCESS'){
+        //         var Result=response.getReturnValue();
+        //         component.set("v.accounts",Result);
+        //     }
+        // });
+        // $A.enqueueAction(action);
+//     }
+// })
