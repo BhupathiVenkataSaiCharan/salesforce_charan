d5350e452ea585eb853da0fef35e441e6e3ab0f6
BhupathiVenkataSaiCharan <venkatasaicharanbhupathi@gmail.com>
2024-07-18 15:31:46 +0530

    custom component refresh on update in standard details page

---
+++ force-app/main/default/lwc/scenario20/scenario20.js
@@ -0,0 +1,26 @@
+import { api, LightningElement, wire } from 'lwc';
+import getAccountRecord from "@salesforce/apex/Scenario20Controller.getAccountRecord";
+import { RefreshEvent } from 'lightning/refresh';
+import {refreshApex} from '@salesforce/apex'
+
+export default class Scenario20 extends LightningElement {
+    @api recordId;
+    data;
+    error;
+    updatedData;
+
+    renderedCallback(){
+        refreshApex(this.updatedData);
+    }
+
+    @wire(getAccountRecord,{accId : '$recordId'})
+    wiredData(result){
+        this.updatedData = result;
+        const {data,error} = result;
+        if(data){
+            this.data = data;
+        }else{
+            this.error = error;
+        }
+    }
+}
