ec152459c0f55ec2f2b0c01c988bd1ad47abb3d5
BhupathiVenkataSaiCharan <venkatasaicharanbhupathi@gmail.com>
2024-09-25 16:57:53 +0530

    account related contacts count and account record page redirect

---
+++ force-app/main/default/lwc/accRelatedConCount/accRelatedConCount.js
@@ -0,0 +1,40 @@
+import { LightningElement, wire } from 'lwc';
+import conCount from '@salesforce/apex/AccRelatedConCountController.conCount';
+
+export default class AccRelatedConCount extends LightningElement {
+
+    data = [];
+    error;
+    headers = [
+        {
+            label : 'Account Name', 
+            fieldName : 'accountUrl', 
+            type : 'url',
+            typeAttributes:{
+                label : {fieldName : 'Name'},
+                target : '_self'
+            }
+        },
+        {label : 'Number of Contacts', fieldName : 'cnt'}
+    ];
+
+    @wire(conCount)
+    wiredCount({data,error}){
+        if(data){
+            // this.data = data;
+            this.data = data.map(row => {
+                return {
+                    ...row,
+                    accountUrl: `/${row.AccountId}` // Construct Account URL 
+                };
+            });
+            console.log('wire data ', this.data);
+        }else{
+            this.error = error;
+        }
+    }
+
+    connectedCallback(){
+        console.log('connectedcallback ');
+    }
+}
